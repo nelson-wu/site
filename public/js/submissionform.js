6f054af1c4ce3ad1a70f29de6b4b38617323c4ac
Nelson Wu <wu_nelson@outlook.com>
2016-09-16 21:05:47 -0400

    user login finished

--- public/js/submissionform.js
+++ public/js/submissionform.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $('#login-form').submit(function(event) {
         event.preventDefault();
-        console.log($(this).serialize());
 
         $.post('/php/login.php', $(this).serialize())
             .done(function(response) {
-                console.log(response);
-                if (response.text === 'success'){
+                response = $.parseJSON(response);
+                if (response.text == 'success'){
                     $('#content').empty()
                         .load('/php/loadForm.php');
+                } else {
+                    console.log('mismatch');
                 }
             });
     });
